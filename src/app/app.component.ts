0449faac82c5d78837c689ce03a942260e432937
Christian Krivickis <S00198911@mail.itsligo.ie>
2022-02-15 12:12:52 +0000

    Lab 3 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseApiService } from './firebase-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'book';
+  MyBooks: any = [];
+  titleValue = "";
+  authorValue = "";
+  title = "Book Firebase app";
+
+  constructor(public firebaseApiService : FirebaseApiService) { }
+
+  ngOnOnit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    return this.firebaseApiService.getBooks().subscribe((data: {}) => {
+      this.MyBooks = data;
+    })
+  }
+
+  addBook() {
+    return this.firebaseApiService.addBook(this.titleValue, this.authorValue).subscribe((data: {}) => {
+      this.MyBooks = data;
+      this.titleValue = "";
+      this.authorValue = "";
+    })
+  }
+
+  deleteBook(id: string){
+    return this.firebaseApiService.deleteBook(id).subscribe((data: {}) => {
+      this.MyBooks = data;
+    })
+  }
 }
